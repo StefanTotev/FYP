965efca83f44b11e391a66ff3856256b8296c4b0
Stefan Totev <Stefan Totev>
2017-04-17 15:41:06 +0100

    Final versio

--- checkLogIn.js
+++
@@ -1,7 +0,0 @@
-var getCookiesDetails = {"url": "http://developer.chrome.com/extensions/cookies.html",
-    "name": "userID"}
-chrome.cookies.get(getCookiesDetails, function (cookie) {
-    if(cookie == null) {
-        window.location.replace("popup.html");
-    }
-});
--- myScript.js
+++
@@ -1,7 +0,0 @@
-chrome.extension.sendRequest(window.location.hostname);
-
-chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-    if (msg.action == 'getURL') {
-        chrome.extension.sendRequest(window.location.hostname);
-    }
-});
